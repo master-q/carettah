821b239e006a02e5933f4ef05be2d804e1da4472
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-07-21 12:13:13 +0900

    support pandoc markdown with BUG.

--- Carettah.hs
+++ Carettah.hs
@@ -1,6 +1,8 @@
 import qualified Graphics.UI.Gtk as G
 import qualified Graphics.Rendering.Cairo as C
 
+import System
+
 import Data.Char
 import Data.Bits
 import Data.IORef
@@ -9,13 +11,19 @@ import Control.Monad.Reader
 
 import Control.Monad.Trans (liftIO)
 
+import qualified Text.Pandoc as P
+import qualified System.IO.UTF8 as U
+
+markdown :: String -> P.Pandoc
+markdown = P.readMarkdown P.defaultParserState{ P.stateStandalone = True }
+
 type PresenSlide = [C.Render ()]
 
 -- global value
-data CarettahState = CarettahState { page :: Int }
+data CarettahState = CarettahState { page :: Int, slides :: [PresenSlide] }
 
 carettahState :: IORef CarettahState
-carettahState = unsafePerformIO $ newIORef CarettahState { page = 0 }
+carettahState = unsafePerformIO $ newIORef CarettahState { page = 0, slides = undefined }
 
 updateCarettahState :: MonadIO m => (CarettahState -> CarettahState) -> m ()
 updateCarettahState fn = liftIO $! atomicModifyIORef carettahState $ \st -> (fn st, ())
@@ -27,23 +35,15 @@ updatePage :: MonadIO m => (Int -> Int) -> m ()
 updatePage fn = updateCarettahState (\s -> s { page = fn $ page s })
 
 nextPage, prevPage :: MonadIO m => m ()
-nextPage = let max = length myPresentation - 1
-           in updatePage $ (\p -> if p >= max then max else p + 1)
-prevPage = updatePage $ (\p -> if p == 0 then 0 else p - 1)
+nextPage = do s <- queryCarettahState slides
+              let max = length s - 1
+              updatePage (\p -> if p >= max then max else p + 1)
+prevPage = updatePage (\p -> if p == 0 then 0 else p - 1)
+
+updateSlides :: MonadIO m => ([PresenSlide] -> [PresenSlide]) -> m ()
+updateSlides fn = updateCarettahState (\s -> s { slides = fn $ slides s })
 
 -- posX,posY,fsizeの値は640x480の画面サイズが基準
-myPresentation :: [PresenSlide]
-myPresentation = [
-  -- 0
-  [renderPngFit 0.3 "start_haskell.png",
-   renderTextCenter 200 55 "C言語の世界と仲良しに",
-   renderTextCenter 300 30 "Kiwamu Okabe"],
-  -- 1
-  [renderTextCenter 100 40 "じゃじゃーんプレゼンツール作ったヨー"],
-  -- 2
-  [renderTextCenter 100 40 "内容どないっしょ"]
-  ]
-
 windowWidth, windowHeight :: Int
 windowWidth   = 640
 windowHeight  = 480
@@ -85,7 +85,7 @@ renderText x y fsize text = do
   C.save
   mySetFontSize fsize
   C.moveTo x y
-  C.textPath $ toUTF $ text
+  C.textPath $ toUTF text
   C.fill
   C.stroke
   C.restore
@@ -125,20 +125,57 @@ clearCanvas w h = do
   C.restore
 
 renderSlide :: Int -> Int -> Int -> C.Render ()
-renderSlide s w h = do
+renderSlide p w h = do
+  s <- queryCarettahState slides
   clearCanvas w h
   C.scale (toDouble w / toDouble windowWidth) (toDouble h / toDouble windowHeight)
-  mapM_ id (myPresentation !! s)
+  mapM_ id (s !! p)
+
+splitBlocks :: P.Pandoc -> [[P.Block]]
+splitBlocks (P.Pandoc _ blocks) = go blocks
+  where go (P.Header 1 h:xs) = let (b1, b2) = break (\a -> case a of
+                                                      (P.Header 1 _) -> True
+                                                      _ -> False) xs
+                             in (P.Header 1 h:b1):go b2
+        go _ = []
+
+backgroundTop :: [P.Block] -> [P.Block]
+backgroundTop blocks = filter go blocks ++ filter (not . go) blocks
+  where go (P.Para [P.Image [P.Str "background"] _]) = True
+        go _ = False
+
+inlinesToString :: [P.Inline] -> String
+inlinesToString = foldr go ""
+  where go (P.Str s) a = s ++ a
+        go P.Space a = " " ++ a
+        go x _ = show x
+
+blockToSlide :: [P.Block] -> PresenSlide
+blockToSlide blockss = map go blockss
+  where
+    go (P.Para [P.Image [P.Str "background"] (pngfile, _)]) = 
+      renderPngFit 0.3 pngfile
+    go (P.Header 1 strs) =
+      renderTextCenter 100 40 (inlinesToString strs)
+    go (P.BulletList plains) = 
+      renderTextCenter 300 30 (show plains) -- xxxx
+    go (P.Para strs) = renderTextCenter 300 30 (inlinesToString strs)
+    go x = error $ show x -- markdownで書いたもの一部のみをサポート
 
 main :: IO ()
 main = do
+  -- parse markdown
+  args <- getArgs
+  s <- case args of
+    (x:_) -> U.readFile x
+    _     -> error "*** Need markdown filename."
+  updateSlides $ const $ map (blockToSlide . backgroundTop) $ splitBlocks $ markdown s
+  -- start GUI
   _ <- G.initGUI
   window <- G.windowNew
   canvas <- G.drawingAreaNew
-  -- fix size
-  --   G.windowSetResizable window False
   G.widgetSetSizeRequest window windowWidth windowHeight
-  -- press key
+  -- key event
   window `G.on` G.keyPressEvent $ G.tryEvent $ do
     keyName <- G.eventKeyName
     liftIO $
@@ -150,6 +187,7 @@ main = do
                   updateCanvas canvas
         "k" -> do prevPage
                   updateCanvas canvas
+        "r" -> print "reload" -- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
   _ <- G.onDestroy window G.mainQuit
   G.onExpose canvas $ const (do updateCanvas canvas
                                 return True)
---
+++ sample.md
@@ -0,0 +1,15 @@
+# C言語の世界と仲良しに
+
+![background](start_haskell.png)
+
+Kiwamu Okabe
+
+# HaskellでGUI
+
+できるんです。本当です。
+
+# プレゼンツールの売り
+
+* pandocで書ける
+* wiiリモコン使える
+* フォント綺麗？
