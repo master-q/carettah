6f0b8473127e112f7f42b17df2db20cd4bdc727d
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-05-28 22:49:31 +0900

    Add "+RTS", "-V0" to command option

--- Runner.hs
+++ Runner.hs
@@ -1,2 +1,11 @@
+import System.FilePath
+import System.Environment
+import System.Cmd
+import Paths_carettah (getBinDir)
+
 main :: IO ()
-main = return ()
+main = do
+  as <- getArgs
+  d <- getBinDir
+  _ <- rawSystem (d </> "_carettah_main_") (as ++ ["+RTS", "-V0"])
+  return ()
